471a58ea94b2690a1b3811654526cc2b8d8a5dab
iday <johnny.iday@qq.com>
2018-05-17 13:35:06 +0800

    修复异常

--- android/src/main/java/com/reactnativecomponent/amaplocation/RCTAMapLocationModule.java
+++ android/src/main/java/com/reactnativecomponent/amaplocation/RCTAMapLocationModule.java
@@ -268,7 +268,7 @@ public class RCTAMapLocationModule extends ReactContextBaseJavaModule {
                     int status = bundle.getInt(GeoFence.BUNDLE_KEY_FENCESTATUS);
                     //获取自定义的围栏标识：
                     String cId = bundle.getString(GeoFence.BUNDLE_KEY_CUSTOMID);
-                    callback.invoke();
+                    callback.invoke(null);
                     //获取当前有触发的围栏对象：
                     GeoFence fence = bundle.getParcelable(GeoFence.BUNDLE_KEY_FENCE);
                     mGeoFenceClient.removeGeoFence(fence);
